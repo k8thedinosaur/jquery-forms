53758e48210ff02a543f6a55bdff16d0e18f63ef
Kate Farkas <kate.m.farkas@gmail.com>
2020-03-25 08:47:59 -0700

    adds buggy code example to practice debugging

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,34 @@
+// $(document).ready(function() {
+//   $("#formOne").submit(function() {
+//     var person1Input = $("input#person1").val();
+//     var person2Input = $("input#person2").val();
+//     var animalInput = $("input#animal").val();
+//     var exclamationInput = $("input#exclamation").val();
+//     var verbInput = $("input#verb").val();
+//     var noun1Input = $("input#noun").val();
+
+//     $(".person1").text(person1Input);
+//     $(".person2").text(person2Input);
+//     $(".animal").text(animalInput);
+//     $(".exclamation").text(exclamationInput);
+//     $(".verb").text(verbInput);
+//     $(".noun").text(noun1Input);
+
+//     $("#story").show();
+
+//     event.preventDefault();
+//   });
+// });
+
 $(document).ready(function() {
-  $("#formOne").submit(function() {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput = $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var noun1Input = $("input#noun").val();
+  $("#blanks form").submit(function() {
+    var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(noun1Input);
+    blanks.forEach(function(blank) {
+      var userInput = $("input." + blank).val();
+      $("." + blank).text(userInput).val();      
+    });
 
-    $("#story").show();
-
-    event.preventDefault();
+    $("#story").sho();
   });
 });
